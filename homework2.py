d948de6a906fd03c6afa9f3c550d73cd0fade901
Alirisiona <ALIRISIONA@GMAIL.COM>
2024-07-10 21:37:56 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+kolichestvo_vypolnennyh_zadach = 12
+print (kolichestvo_vypolnennyh_zadach)
+kolichestvo_zatrachennyh_chasov = 1.5
+print (kolichestvo_zatrachennyh_chasov)
+nazvanie_kursa = "Python"
+print (nazvanie_kursa)
+vremya_na_odno_zadanie = kolichestvo_zatrachennyh_chasov / kolichestvo_vypolnennyh_zadach
+print (vremya_na_odno_zadanie)
+print ("kurs:",nazvanie_kursa,",","vsego zadach:",kolichestvo_vypolnennyh_zadach,",",
+       "zatracheno chasov:",kolichestvo_zatrachennyh_chasov,",","srednee vremya vypolnenia:",vremya_na_odno_zadanie)
